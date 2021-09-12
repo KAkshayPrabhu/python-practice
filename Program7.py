189d4049f5a324505364880fc74f092237f7bb4a
Akshay Prabhu <90515389+kakshayprabhu@users.noreply.github.com>
2021-09-12 14:34:52 +0530

    String Manipulation program

---
+++ Program7.py
@@ -0,0 +1,10 @@
+text = "Kannada  is a Dravidian language spoken predominantly by the people " \
+       "of Karnataka in the southwestern region of India"
+
+print(text.find("D"))
+print(text.find("spoken"))
+
+print(text.replace("Karnataka", "Karunadu"))
+print('India' in text)
+
+print(text.lower().upper())
