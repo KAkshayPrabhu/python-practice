f5f29f6a32a12a147a5fb760fc62aaa365bcd769
Akshay Prabhu <90515389+kakshayprabhu@users.noreply.github.com>
2021-09-12 15:12:27 +0530

    Foramtted string program

--- Program6.py
+++ Program6.py
@@ -1,6 +1,6 @@
 first = "MicroDegree"
 
-print("Hello There" + first + "is a tutorial site 1")
+print("Hello There " + first + " is a tutorial site 1")
 
 print (f"Hello there {first} is a tutorial site 2" )
 
