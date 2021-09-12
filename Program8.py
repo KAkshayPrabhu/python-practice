0040f3affc0185248b79081e69c693e0397c709c
Akshay Prabhu <90515389+kakshayprabhu@users.noreply.github.com>
2021-09-12 14:57:35 +0530

    Random functions

---
+++ Program8.py
@@ -0,0 +1,18 @@
+total = -290.25
+
+print("Total is "+ str(total))
+
+#total = total + 12
+total += 12
+
+print(total)
+print(round(total))
+print(abs(total))
+
+print(pow(3,2))
+print(max(11,5))
+
+import math
+print(math.ceil(1234.412))
+print(math.floor(1234.412))
+print(math.sqrt(25))
