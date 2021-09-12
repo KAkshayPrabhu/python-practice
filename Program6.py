93a781c83993952aebd080952ab694e73a105fe5
Akshay Prabhu <90515389+kakshayprabhu@users.noreply.github.com>
2021-09-12 14:13:32 +0530

    Formatted String program

---
+++ Program6.py
@@ -0,0 +1,9 @@
+first = "MicroDegree"
+
+print("Hello There" + first + "is a tutorial site 1")
+
+print (f"Hello there {first} is a tutorial site 2" )
+
+variable = f"Hello there {first} is a tutorial site 3"
+
+print(variable)
