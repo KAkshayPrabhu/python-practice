d156d7705aebcc6f0667048b84a6094953b118de
Akshay Prabhu <90515389+kakshayprabhu@users.noreply.github.com>
2021-09-12 13:47:21 +0530

    Type Conversion Program

---
+++ Program5.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter first number:"))
+second_number = int(input("Enter second number:"))
+
+print("First Number:", first_number)
+print("Second Number", second_number)
+
+total_sum = first_number + second_number
+print("Sum:", total_sum)
